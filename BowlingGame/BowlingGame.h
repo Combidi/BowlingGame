5f80966e0f6aa41efa414ac2dfa5c24bcb5fd25d
Peter Combee <petercombee@gmail.com>
2023-08-17 17:01:53 +0200

    Initial Commit

---
+++ BowlingGame/BowlingGame.h
@@ -0,0 +1,18 @@
+//
+//  BowlingGame.h
+//  BowlingGame
+//
+//  Created by Peter Combee on 17/08/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BowlingGame.
+FOUNDATION_EXPORT double BowlingGameVersionNumber;
+
+//! Project version string for BowlingGame.
+FOUNDATION_EXPORT const unsigned char BowlingGameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BowlingGame/PublicHeader.h>
+
+
