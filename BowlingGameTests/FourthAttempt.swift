d35a0945580b26b21620986a487bddf9e96f62e0
Peter Combee <petercombee@gmail.com>
2023-10-23 20:15:43 +0200

    `BowlingGame.score for rolling one gutter ball, delivers score of zero

--- BowlingGameTests/FourthAttempt.swift
+++ BowlingGameTests/FourthAttempt.swift
@@ -1,8 +1,21 @@
 //
-//  FourthAttempt.swift
-//  BowlingGameTests
-//
 //  Created by Peter Combee on 23/10/2023.
 //
 
-import Foundation
+import XCTest
+
+final class FourthAttemptTests: XCTestCase {
+    
+    struct BowlingGame {
+        func roll(pins: Int) {}
+        func score() -> Int { 0 }
+    }
+    
+    func test_rollOneGutterBall_deliversScoreOnZero() {
+        let sut = BowlingGame()
+
+        sut.roll(pins: 0)
+        
+        XCTAssertEqual(sut.score(), 0)
+    }
+}
